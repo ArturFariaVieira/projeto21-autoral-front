172f8a93e9ddbe60d04b11ef5fdb8d43add35623
Artur Faria Vieira <artur.f.vieira.94@gmail.com>
2023-03-29 19:52:46 -0300

    start and configs

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+import './assets/styles/reset.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
